aee0667675ad4cfe55ae6d3f53e3526a5f7fd467
yura <yur4ikkr@gmail.com>
2023-06-20 01:09:18 +0300

    Client :: unit test :: fix

--- examples/get.go
+++ examples/get.go
@@ -1,15 +1,10 @@
 package examples
 
-import "fmt"
-
 func Get() (string, error) {
 	res, err := HttpClient.Get("https://webhook.site/2c52f051-5e9f-458e-8e4d-4cf44fff1ada", nil)
 	if err != nil {
 		return "", err
 	}
-
-	fmt.Println(res.BodyString())
-	fmt.Println(res.GetStatusCode())
 
 	return res.BodyString(), nil
 }
--- gohttp/mock_server.go
+++ gohttp/mock_server.go
@@ -1,6 +1,9 @@
 package gohttp
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"strings"
 	"sync"
 )
 
@@ -30,16 +33,38 @@ func StopMockServer() {
 	mockupServer.enabled = false
 }
 
+func FlushMocks() {
+	mockupServer.serverMutex.Lock()
+	defer mockupServer.serverMutex.Unlock()
+
+	mockupServer.mocks = make(map[string]*Mock)
+}
+
 func AddMock(mock Mock) {
 	mockupServer.serverMutex.Lock()
 	defer mockupServer.serverMutex.Unlock()
 
 	key := mockupServer.getMockKey(mock.Method, mock.Url, mock.RequestBody)
+
 	mockupServer.mocks[key] = &mock
 }
 
 func (m *mockServer) getMockKey(method string, url string, body string) string {
-	return method + url + body
+	hasher := md5.New()
+	hasher.Write([]byte(method + url + m.cleanBody(body)))
+
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+func (m *mockServer) cleanBody(body string) string {
+	body = strings.TrimSpace(body)
+	if body == "" {
+		return ""
+	}
+
+	body = strings.ReplaceAll(body, "\t", "")
+	body = strings.ReplaceAll(body, "\n", "")
+
+	return body
 }
 
 func (m *mockServer) getMock(method string, url string, body string) *Mock {
